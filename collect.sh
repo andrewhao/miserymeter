0ba45b97ad9c96c830126063c667f0d9148d4afa
Andrew Hao <andrewhao@gmail.com>
2018-02-12 22:18:00 -0800

    Script changes

--- collect.sh
+++ collect.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-soundmeter -s 10 --collect
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RECORDING_FILE="${DIR}/recordings/sample.wav"
+arecord --device=hw:1,0 --format S16_LE --rate 48000 -c1 -d 5 --quiet "${RECORDING_FILE}"
+sox -V3 ${RECORDING_FILE} -n stats 2>&1 | grep dB
