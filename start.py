6c54a467c23e945df383e3a9529f2e0ce7aa8588
Andrew Hao <andrewhao@gmail.com>
2018-02-12 23:18:14 -0800

    Test reporting tools

---
+++ start.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import requests
+from subprocess import Popen, PIPE
+import os
+print os.getcwd()
+
+p = Popen([os.getcwd() + "/test.sh"], stdout=PIPE)
+raw_output = p.communicate()[0].split('\n')
+trimmed = map(lambda entry: entry.split(), raw_output)
+filtered = filter(lambda entry: len(entry) > 0, trimmed)
+separated = map(lambda entry: dict(label=" ".join(entry[:-1]), value=entry[-1]), filtered)
+data = {}
+for entry in separated:
+    label = entry["label"].lower().replace(" ", "_")
+    data[label] = entry["value"]
+print data
+
+data.update(dict(device_id='rpi2'))
+
+response = requests.post('https://thermonoto.herokuapp.com/ambient_noise_updates', data=data)
+print('Posting with', data)
+print(response)
