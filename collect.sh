ae4a7fec84f5cb0122694ce43ebf66a8b12992ad
Andrew Hao <andrewhao@gmail.com>
2018-02-12 23:37:49 -0800

    Bump up recording length

--- collect.sh
+++ collect.sh
@@ -4,5 +4,5 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 RECORDING_FILE="${DIR}/recordings/sample.wav"
-arecord --device=hw:1,0 --format S16_LE --rate 48000 -c1 -d 5 --quiet "${RECORDING_FILE}"
+arecord --device=hw:1,0 --format S16_LE --rate 48000 -c1 -d 30 --quiet "${RECORDING_FILE}"
 sox -V3 ${RECORDING_FILE} -n stats 2>&1 | grep dB
