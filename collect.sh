80207e3acbc4df441753f6758318ddaf87462fbe
Andrew Hao <andrewhao@gmail.com>
2018-03-14 20:40:57 -0700

    Adding scp upload of recorded file

--- collect.sh
+++ collect.sh
@@ -3,9 +3,9 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 RECORDING_FILE="${DIR}/recordings/sample.wav"
 DATE=$(date "+%Y-%m-%d-%H:%M:%S")
 UPLOAD_RECORDING_FILENAME="${DATE}.wav"
 arecord --device=hw:1,0 --format S16_LE --rate 48000 -c1 -d 30 --quiet "${RECORDING_FILE}"
+scp ${TENSORFLOW_USER}@${TENSORFLOW_HOST}:~/recordings/${UPLOAD_RECORDING_FILENAME}
 sox -V3 ${RECORDING_FILE} -n stats 2>&1 | grep dB
