650d00e64cf83314fc189ed1d328778d25db608e
Andrew Hao <andrewhao@gmail.com>
2018-03-15 23:59:20 -0700

    Use ISO format

--- collect.sh
+++ collect.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 RECORDING_FILE="${DIR}/recordings/sample.wav"
-DATE=$(date "+%Y-%m-%d-%H:%M:%S")
+DATE=$(date "+%Y-%m-%d %H:%M:%S")
 UPLOAD_RECORDING_FILENAME="${DATE}.wav"
 arecord --device=hw:1,0 --format S16_LE --rate 48000 -c1 -d 30 --quiet "${RECORDING_FILE}"
 scp ${RECORDING_FILE} ${TENSORFLOW_USER}@${TENSORFLOW_HOST}:~/recordings/${UPLOAD_RECORDING_FILENAME}
